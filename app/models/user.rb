d0337753ec76bb8ccc10c237a9a72898b8dbcfc0
doupi0331 <lisau4mp6@gmail.com>
2017-01-15 12:43:12 +0800

    Add payment functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment # 只要user更動，payment也會一併更動
 end
